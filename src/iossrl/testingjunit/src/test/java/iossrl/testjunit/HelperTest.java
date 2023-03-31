0cc6922bc6556c0af542b57a909e1a9df3b2d761
Marco Mero <94mero@gmail.com>
2023-03-31 12:03:49 +0200

    testing JUnit failures

--- src/iossrl/testingjunit/src/test/java/iossrl/testjunit/HelperTest.java
+++ src/iossrl/testingjunit/src/test/java/iossrl/testjunit/HelperTest.java
@@ -9,6 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  * correctly.
  */
 public class HelperTest {
+
     /**
      * This simple test is just run to determine whether Junit is running at all.
      */
@@ -17,4 +18,15 @@ public class HelperTest {
         int result = Helper.helperMethod();
         assertEquals(2, result);
     }
+
+    /**
+     * This simple test should fail, showing how JUnit behaves on failure.
+     */
+    @Test
+    void testHelperMethodFAILURE() {
+        int result = Helper.helperMethod();
+
+        assertEquals(result - 1, result);
+    }
+
 }
