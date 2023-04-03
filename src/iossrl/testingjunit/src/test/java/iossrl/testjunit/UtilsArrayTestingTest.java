e1995a3674973645235221ff0d8002a27298bc11
Marco Mero <94mero@gmail.com>
2023-04-03 10:30:15 +0200

    Added printArr test

--- src/iossrl/testingjunit/src/test/java/iossrl/testjunit/UtilsArrayTestingTest.java
+++ src/iossrl/testingjunit/src/test/java/iossrl/testjunit/UtilsArrayTestingTest.java
@@ -1,21 +1,49 @@
 package iossrl.testjunit;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * This class tests my UtilsArrayTesting class.
  */
+@SuppressWarnings("checkstyle:MagicNumber")
 public class UtilsArrayTestingTest {
     /**
-     * no.
+     * *stringToIntArray()
+     * Testing reformatting.
      */
     @Test
-    @SuppressWarnings("checkstyle:MagicNumber")
-    void testStringToIntArray() {
+    void baseStringToIntArray() {
         int[] expected = new int[] {0, 1, 2, 3, 4};
         int[] result = UtilsArrayTesting.stringToIntArray("[ 0, 1, 2, 3, 4]");
         assertArrayEquals(expected, result);
     }
+
+    /**
+     * *stringToIntArray()
+     * Testing empty string.
+     */
+    void emptyStringToIntArray() {
+        int[] expected = new int[0];
+        int[] result = UtilsArrayTesting.stringToIntArray("");
+        assertArrayEquals(expected, result);
+    }
+
+    /**
+     * *printArr()
+     * base case for printing tests.
+     */
+    @Test
+    public void testPrintArr() {
+        int[] intArray = {1, 2, 3, 4};
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        UtilsArrayTesting.printArr(intArray);
+        assertEquals("[1 2 3 4]", outContent.toString());
+    }
+
 }
